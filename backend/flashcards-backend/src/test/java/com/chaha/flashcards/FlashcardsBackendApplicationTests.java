e4b46cd20939b8788d7ab260367f4ac326060a2d
Chahat <chahatnandrajog@gmail.com>
2026-03-12 02:54:16 -0400

    initialize Spring Boot backend and configure PostgreSQL

---
+++ backend/flashcards-backend/src/test/java/com/chaha/flashcards/FlashcardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaha.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
